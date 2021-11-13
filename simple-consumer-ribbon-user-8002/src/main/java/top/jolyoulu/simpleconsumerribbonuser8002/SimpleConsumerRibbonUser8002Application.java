96d72be593a7520e3cfb4c3c33830036cca4231e
JolyouLu <1307633126@qq.com>
2021-11-13 17:21:54 +0800

    simple-consumer-ribbon-user-8002

---
+++ simple-consumer-ribbon-user-8002/src/main/java/top/jolyoulu/simpleconsumerribbonuser8002/SimpleConsumerRibbonUser8002Application.java
@@ -0,0 +1,13 @@
+package top.jolyoulu.simpleconsumerribbonuser8002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleConsumerRibbonUser8002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleConsumerRibbonUser8002Application.class, args);
+    }
+
+}
---
+++ simple-consumer-ribbon-user-8002/src/test/java/top/jolyoulu/simpleconsumerribbonuser8002/SimpleConsumerRibbonUser8002ApplicationTests.java
@@ -0,0 +1,9 @@
+package top.jolyoulu.simpleconsumerribbonuser8002;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleConsumerRibbonUser8002ApplicationTests {
+
+
+}
