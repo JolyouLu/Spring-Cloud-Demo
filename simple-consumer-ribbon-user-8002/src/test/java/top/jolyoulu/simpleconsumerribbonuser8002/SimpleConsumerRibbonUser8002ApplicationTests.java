4c77741a1f626757f4a86a7da4ee13bc554a1e1d
JolyouLu <1307633126@qq.com>
2021-11-15 23:00:42 +0800

    重新构建项目

--- simple-consumer-ribbon-user-8002/src/test/java/top/jolyoulu/simpleconsumerribbonuser8002/SimpleConsumerRibbonUser8002ApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package top.jolyoulu.simpleconsumerribbonuser8002;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SimpleConsumerRibbonUser8002ApplicationTests {
-
-
-}
